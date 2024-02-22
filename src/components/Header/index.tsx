c9c836889477360fa8ab11acab9c07de42b3a726
Priscilla <priscillaonil@gmail.com>
2024-02-21 21:52:15 -0300

    feat: component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,24 +4,32 @@ import { formattedHour } from "../../utils/hour";
 
 export function Header() {
   return (
-    <header className="d-flex gap-5">
-      <div className="d-flex flex-column">
-        <Logo color="#00A868" />
+    <header className="row gap-5">
+      <div className="col-2">
+        <div className="d-flex flex-column gap-1">
+          <Logo color="#00A868" />
 
-        <div className="d-flex align-items-center gap-2">
-          <div
-            className="bg-success w-75 opacity-50"
-            style={{ height: "4px" }}
-          ></div>
+          <div className="d-flex align-items-center gap-2">
+            <div
+              className="bg-success w-75 opacity-50"
+              style={{ height: "4px" }}
+            />
 
-          <span className="text-success fw-bold">Currency</span>
+            <span className="text-success fw-bold">Currency</span>
+          </div>
         </div>
       </div>
 
-      <div>
-        <p className="fs-5 fw-medium m-0">{`${formattedDate()} | ${formattedHour()}`}</p>
+      <div className="col-5">
+        <div className="d-flex flex-column gap-1">
+          <p className="fs-5 fw-medium m-0">
+            {`${formattedDate()} | ${formattedHour()}`}
+          </p>
 
-        <p>Dados de câmbio disponibilizados pela Morningstar.</p>
+          <p className="m-0">
+            Dados de câmbio disponibilizados pela Morningstar.
+          </p>
+        </div>
       </div>
     </header>
   );
