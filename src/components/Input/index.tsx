cba37ce372bb0117576549afb936b8605ee659b7
Priscilla <priscillaonil@gmail.com>
2024-02-27 20:57:36 -0300

    feat: add new maskInputMoney

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,40 @@
-import { NumericFormat } from "react-number-format";
+// import { NumericFormat } from "react-number-format";
 import { InputProps } from "./types";
 
 export function Input({ name, placeholder, onChange, value }: InputProps) {
+  const maskMoneyInput = (input: string) => {
+    //Garante que só vai ter números
+    let formattedValue = input.replace(/\D/g, "");
+
+    //Adiciona virgula para criar os centavos
+    formattedValue = formattedValue.replace(/(\d+)(\d{2})$/, "$1,$2");
+
+    //Adiciona ponto para criar as centenas
+    formattedValue = formattedValue.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+
+    //Insere o cifrão na frente do valor
+    formattedValue = "$" + " " + formattedValue;
+
+    return formattedValue;
+  };
+
   return (
     <div className="d-flex flex-column">
       <label htmlFor={name} className="fw-medium">
         {placeholder}
       </label>
 
-      <NumericFormat
+      <input
+        id={name}
+        className="border p-2 rounded shadow-sm"
+        name={name}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          onChange(maskMoneyInput(event.target.value))
+        }
+        value={maskMoneyInput(value)}
+      />
+
+      {/* <NumericFormat
         className="border p-2 rounded shadow-sm"
         thousandSeparator=","
         prefix="$ "
@@ -18,7 +44,7 @@ export function Input({ name, placeholder, onChange, value }: InputProps) {
           onChange(event.target.value)
         }
         value={value}
-      />
+      /> */}
     </div>
   );
 }
