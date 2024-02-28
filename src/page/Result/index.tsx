abc0fdc0a70b84cdba09b54ce04350043eb54dcf
Priscilla <priscillaonil@gmail.com>
2024-02-28 17:44:28 -0300

    feat: test route result

--- src/page/Result/index.tsx
+++ src/page/Result/index.tsx
@@ -1,49 +1,48 @@
-// import { Header } from "../../components/Header";
-// import { Button } from "../../components/Button";
-// import { formattedPrice } from "../../utils/formattedPrice";
-// import { useResult } from "./useResult";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { formattedPrice } from "../../utils/formattedPrice";
+import { useResult } from "./useResult";
 
 export function Result() {
-  // const { handleBackToStart, totalValue, valueDollar, name, taxa } =
-  //   useResult();
+  const { handleBackToStart, totalValue, valueDollar, name, taxa } =
+    useResult();
 
   return (
-    <div>OI</div>
-    // <div className="background-default">
-    //   <div className="container d-flex flex-column gap-2 gap-lg-5 pt-5">
-    //     <Header />
+    <div className="background-default">
+      <div className="container d-flex flex-column gap-2 gap-lg-5 pt-5">
+        <Header />
 
-    //     <main className="d-flex flex-column gap-4 mt-5">
-    //       <Button
-    //         styleType="btn-light shadow-sm bg-transparent"
-    //         icon="arrowLeft"
-    //         onClick={handleBackToStart}
-    //       >
-    //         Voltar
-    //       </Button>
+        <main className="d-flex flex-column gap-4 mt-5">
+          <Button
+            styleType="btn-light shadow-sm bg-transparent"
+            icon="arrowLeft"
+            onClick={handleBackToStart}
+          >
+            Voltar
+          </Button>
 
-    //       <div>
-    //         <p className="text-light-emphasis fw-bolder">
-    //           O resultado do cálculo é
-    //         </p>
+          <div>
+            <p className="text-light-emphasis fw-bolder">
+              O resultado do cálculo é
+            </p>
 
-    //         <h2 className="fw-medium fs-1 text-success">
-    //           {formattedPrice(totalValue)}
-    //         </h2>
-    //       </div>
+            <h2 className="fw-medium fs-1 text-success">
+              {formattedPrice(totalValue)}
+            </h2>
+          </div>
 
-    //       <div>
-    //         <p className="text-light-emphasis">
-    //           Compra no <span className="fw-medium">{name}</span> e taxa de{" "}
-    //           <span className="fw-medium">{taxa}</span>
-    //         </p>
-    //         <p className="text-light-emphasis">
-    //           <span className="fw-medium">Cotação do dólar: </span>
-    //           {formattedPrice(valueDollar)}
-    //         </p>
-    //       </div>
-    //     </main>
-    //   </div>
-    // </div>
+          <div>
+            <p className="text-light-emphasis">
+              Compra no <span className="fw-medium">{name}</span> e taxa de{" "}
+              <span className="fw-medium">{taxa}</span>
+            </p>
+            <p className="text-light-emphasis">
+              <span className="fw-medium">Cotação do dólar: </span>
+              {formattedPrice(valueDollar)}
+            </p>
+          </div>
+        </main>
+      </div>
+    </div>
   );
 }
